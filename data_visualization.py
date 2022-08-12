6e7e5dcb197533789e6d199433ebb767c27f719a
vssjunior <valdecisoares23@yahoo.com.br>
2022-08-12 16:57:56 -0300

    graphics

---
+++ data_visualization.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+#import matplotlib as mpl
+import numpy as np
+
+x = [1,2]
+y = [2,3]
+plt.plot(x,y)
+plt.show()
+
+#############
+
+
+# x = np.linspace(0, 20, 100)
+# plt.plot(x, np.sin(x))
+# plt.show()
